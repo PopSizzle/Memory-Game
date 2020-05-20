cfc18b1c6a31a6ab2a06f926946630542c4a63b9
Sam Poppe <poppe.samuel@gmail.com>
2020-05-20 09:46:55 -0700

    Updates

---
+++ precache-manifest.eb5e25e63cb404977a9223c164960b5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b04079759da293d4400aaa7e7349a390",
+    "url": "/Memory-Game/index.html"
+  },
+  {
+    "revision": "cdd480989ec695191dbd",
+    "url": "/Memory-Game/static/css/main.f5ceb646.chunk.css"
+  },
+  {
+    "revision": "6691a6aa086f936e7940",
+    "url": "/Memory-Game/static/js/2.bc1df35c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Memory-Game/static/js/2.bc1df35c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdd480989ec695191dbd",
+    "url": "/Memory-Game/static/js/main.e8d7d384.chunk.js"
+  },
+  {
+    "revision": "a6c84f544da882adb0d1",
+    "url": "/Memory-Game/static/js/runtime-main.0ac5b1ff.js"
+  }
+]);
